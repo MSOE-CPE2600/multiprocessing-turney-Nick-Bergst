248a127b722bc83ea436ce7d5924f721a205824e
Nick-Bergst <bergstn@msoe.edu>
2024-12-12 18:56:16 -0600

    I tried my best :)

--- mandel.c
+++ mandel.c
@@ -13,13 +13,29 @@
 #include <sys/wait.h>
 #include "jpegrw.h"
 #include <time.h>
+#include <pthread.h>
+#include <string.h>
 
 // local routines
 static int iteration_to_color( int i, int max );
 static int iterations_at_point( double x, double y, int max );
 static void compute_image( imgRawImage *img, double xmin, double xmax,
-									double ymin, double ymax, int max );
+									double ymin, double ymax, int max, int sector);
+void *compute_image_thread(void *args);
 static void show_help();
+
+//arg struct
+struct argument {
+	imgRawImage *img;
+	double xmin;
+	double xmax;
+	double ymin;
+	double ymax;
+	int max;
+	int sector;
+	int sCount;
+	char fOut[50];
+};
 
 
 int main( int argc, char *argv[] )
@@ -44,11 +60,14 @@ int main( int argc, char *argv[] )
 	int procRuns = 5;
 	int a = 0; //does not change
 	int b = 0; //does not change
+	int d = 0; //does not change
 	char fileout[50]; //does not change
+	int imgThreads = 1;
+	int framesGen = 50;
 	// For each command line argument given,
 	// override the appropriate configuration value.
 
-	while((c = getopt(argc,argv,"x:y:s:W:H:m:o:h:M:z:p:"))!=-1) {
+	while((c = getopt(argc,argv,"x:y:s:W:H:m:o:h:M:z:p:t:F:"))!=-1) {
 		switch(c) 
 		{
 			case 'x':
@@ -85,6 +104,11 @@ int main( int argc, char *argv[] )
 			case 'p':
 				procRuns = atoi(optarg);
 				break;
+			case 't':
+				imgThreads = atoi(optarg);
+				break;
+			case 'F':
+				framesGen = atoi(optarg);
 		}
 	}
 
@@ -102,7 +126,7 @@ int main( int argc, char *argv[] )
 		setImageCOLOR(img,0);
 
 		// Compute the Mandelbrot image
-		compute_image(img,xcenter-xscale/2,xcenter+xscale/2,ycenter-yscale/2,ycenter+yscale/2,max);
+		compute_image(img,xcenter-xscale/2,xcenter+xscale/2,ycenter-yscale/2,ycenter+yscale/2,max, imgThreads);
 
 		// Save the image in the stated file.
 		storeJpegImageFile(img,outfile);
@@ -117,12 +141,12 @@ int main( int argc, char *argv[] )
 			if (pid == 0) {
 				//printf("pid 0!\n");
 				int leftOver;
-				if (50 % procRuns != 0 && a == procRuns - 1) {
+				if (framesGen % procRuns != 0 && a == procRuns - 1) {
 					leftOver = 50 % procRuns;
 				} else {
 					leftOver = 0;
 				}
-				for (b = a * (int)(50 / procRuns); b <(((int)(50 / procRuns)) * (a + 1)) + leftOver; b++) {
+				for (b = a * (int)(framesGen / procRuns); b <(((int)(framesGen / procRuns)) * (a + 1)) + leftOver; b++) {
 					//int b = 0;
 					//printf("b: %d\n", b);
 					//make a new xscale from the original, and calculate it
@@ -135,12 +159,47 @@ int main( int argc, char *argv[] )
 					printf("mandel: x=%lf y=%lf xscale=%lf yscale=%1f max=%d fileout=%s\n",xcenter,ycenter,nuxscale,yscale,max,fileout);
 					// Create a raw image of the appropriate size.
 					imgRawImage* img = initRawImage(image_width,image_height);
+					//struct to pass args
+					
 					// Fill it in with black
 					setImageCOLOR(img,0);
 					// Compute the Mandelbrot image
-					compute_image(img,xcenter-nuxscale/2,xcenter+nuxscale/2,ycenter-yscale/2,ycenter+yscale/2,max);
+					pthread_t tid[imgThreads];
+					struct argument arg[imgThreads];
+					for (d = 0; d < imgThreads; d++) {
+						//int sparePixel;
+						//if ((int)((nuxscale / imgThreads) - (int)(nuxscale / imgThreads)) != 0 && d == imgThreads - 1) {
+						//	sparePixel = ((nuxscale / imgThreads) - (int)(nuxscale / imgThreads));
+						//} else {
+						//	sparePixel = 0;
+						//}
+						//printf("b4 %f %f %f %f\n", (xcenter-nuxscale/2),(xcenter+nuxscale/2),(ycenter-yscale/2),(ycenter+yscale/2));
+						arg[d].img = img;
+						arg[d].xmin = (xcenter-nuxscale/2) + ((nuxscale / imgThreads) * (d));
+						arg[d].xmax = (xcenter-nuxscale/2) + ((nuxscale / imgThreads) * (d + 1));
+						arg[d].ymin = (ycenter-yscale/2);
+						arg[d].ymax = (ycenter+yscale/2);
+						arg[d].max = max;
+						arg[d].sector = d;
+						arg[d].sCount = imgThreads;
+						strcpy(arg[d].fOut, fileout);
+						printf("go %f %f %f %f %d %d %s %d %d\n",arg[d].xmin,arg[d].xmax,arg[d].ymin,arg[d].ymax,arg[d].max,arg[d].sector,arg[d].fOut,arg[d].img->width,arg[d].img->height);
+						struct argument *argPoint = &arg[d];
+						pthread_create(&tid[d], NULL, &compute_image_thread, (void *)argPoint);
+						img = arg[d].img;
+					}
+					for (d = 0; d < imgThreads; d++) {
+						void *tRet;
+						imgRawImage* fImg[imgThreads];
+						//char temp[50];
+						pthread_join(tid[d], &tRet);
+						fImg[d] = (imgRawImage *)tRet;
+						//sprintf(temp, "test%d.jpg", d);
+						//storeJpegImageFile(img, temp);
+					}
 					// Save the image in the stated file.
 					storeJpegImageFile(img,fileout);
+					freeRawImage(img);
 				}
 				return 1;
 			}
@@ -148,7 +207,7 @@ int main( int argc, char *argv[] )
 		for (a = 0; a < procRuns; a++) {
 			wait(NULL);
 		}
-		printf("\nType \n\tffmpeg -i mandel%cd.jpg [exapmle file name].mpeg\n to compile the movie!\n", 37);
+		printf("\nType\n\tffmpeg -i mandel%cd.jpg [example file name].mpeg\nto compile the movie!\n", 37);
 	}
 	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 	printf ("Total time = %f seconds\n",(end.tv_nsec - begin.tv_nsec) / 1000000000.0 +(end.tv_sec  - begin.tv_sec));
@@ -165,6 +224,7 @@ in the Mandelbrot space, up to a maximum of max.
 
 int iterations_at_point( double x, double y, int max )
 {
+	//printf("(%f, %f)\n",x,y);
 	double x0 = x;
 	double y0 = y;
 
@@ -181,6 +241,8 @@ int iterations_at_point( double x, double y, int max )
 		iter++;
 	}
 
+	//printf("iter: %d\n", iter);
+
 	return iter;
 }
 
@@ -189,7 +251,60 @@ Compute an entire Mandelbrot image, writing each point to the given bitmap.
 Scale the image to the range (xmin-xmax,ymin-ymax), limiting iterations to "max"
 */
 
-void compute_image(imgRawImage* img, double xmin, double xmax, double ymin, double ymax, int max )
+void *compute_image_thread(void *args)
+{
+	struct argument *argz =  (struct argument *)args;
+	
+	int i,j;
+
+	int width = argz->img->width;
+	int height = argz->img->height;
+
+	printf("in %f %f %f %f %d %d %s %d %d\n",argz->xmin,argz->xmax,argz->ymin,argz->ymax,argz->max,argz->sector,argz->fOut,argz->img->width,argz->img->height);
+
+	// For every pixel in the image...
+	printf("scount: %d from %d to %d\n",argz->sCount, (width * argz->sector)/argz->sCount, (width * ((argz->sector) + 1))/argz->sCount);	
+
+	for(j=0;j<height;j++) {
+		//double xPrev = argz->xmin - 1;
+		//double yPrev = argz->ymin + j*(argz->ymax-argz->ymin)/height;;
+
+		for(i = (width * argz->sector)/argz->sCount;i< (width * ((argz->sector) + 1))/argz->sCount;i++) {
+
+			// Determine the point in x,y space for that pixel.
+			double x = argz->xmin + i*(argz->xmax-argz->xmin)/width;
+			double y = argz->ymin + j*(argz->ymax-argz->ymin)/height;
+
+			//if (x <= xPrev) {
+			//	printf("x error! (%f,%f) sect %d prev (%f,%f)\n",x,y,argz->sector,xPrev,yPrev);
+			//} else if (y != yPrev && yPrev != 0) {
+			//	printf("y error! (%f,%f) sect %d prev (%f,%f)\n",x,y,argz->sector,xPrev,yPrev);
+			//}
+			//xPrev = x;
+			//yPrev = y;
+			//if (x < argz-> xmin || i < (width * argz->sector)/argz->sCount) {
+			//	printf("(%f,%f) sect %d start %d\n", x, y, argz->sector,(width * argz->sector)/argz->sCount);
+			//}
+
+			// Compute the iterations at that point.
+			int iters = iterations_at_point(x,y,argz->max);
+			//printf("iters: %d (%f, %f)\n", iters, x, y);
+			//if (argz->sector == 1 && iters == 0) {
+			//	printf("(%f,%f) sect %d iters %d\n", x, y, argz->sector,iters);
+			//}
+
+			// Set the pixel in the bitmap
+			setPixelCOLOR(argz->img,i,j,iteration_to_color(iters,argz->max));
+		}
+	}
+	//args->img = argz->img;
+	// Save the image in the stated file.
+	//storeJpegImageFile(argz->img,argz->fOut);
+	pthread_exit(argz->img);
+	return NULL;
+}
+
+void compute_image(imgRawImage *img, double xmin, double xmax, double ymin, double ymax, int max, int sector)
 {
 	int i,j;
 
